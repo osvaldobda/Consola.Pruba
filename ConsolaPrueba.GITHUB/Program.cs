2213775cfa84334bc314a318dffe9f4be7f1b50d
ocuevas <rolandobda25@gmail.com>
2023-08-02 16:41:59 -0400

    Solicitu hecha a modo de prueba

--- ConsolaPrueba.GITHUB/Program.cs
+++ ConsolaPrueba.GITHUB/Program.cs
@@ -13,7 +13,7 @@ namespace ConsolaPrueba.GITHUB
             Console.WriteLine("Hola esto es una preuba para GITHUB");
             Console.ReadLine();
 
-            Console.WriteLine("Este es un mensaje nuevo");
+            Console.WriteLine("Este es un mensaje para mejora por el analista");
         }
 
      
