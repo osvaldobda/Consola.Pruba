e2742cdd21729cec349be826c57202dbbfafce5e
ocuevas <ocuevas@adess.gob.do>
2023-08-02 13:43:21 -0400

    se agrega el proyecto

---
+++ ConsolaPrueba.GITHUB/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaPrueba.GITHUB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola esto es una preuba para GITHUB");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
